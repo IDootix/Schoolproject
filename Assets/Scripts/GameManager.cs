6b6eb5afbd2572cf683e4c5ab69a48ea55712d55
IDootix <82953309+IDootix@users.noreply.github.com>
2022-04-29 17:43:24 +0200

    Added GameManager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.GameManager;	
+
+public static GameManager instance;
+
+void Awake()
+{
+    if(Instance == null) // If there is no instance already
+    {
+        DontDestroyOnLoad(gameObject); // Keep the GameObject, this component is attached to, across different scenes
+        Instance = this;
+    } else if(Instance != this) // If there is already an instance and it's not `this` instance
+    {
+        Destroy(gameObject); // Destroy the GameObject, this component is attached to
+    }
+}
