d6521bd2fd2a1d682c1a77aa6c4a58954d92d613
IDootix <linusmoellerjojo@gmail.com>
2022-06-06 14:44:27 +0200

    m

--- Assets/Scripts/PlayerFunctions.cs
+++ Assets/Scripts/PlayerFunctions.cs
@@ -1,51 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 
 public class PlayerFunctions : MonoBehaviour
 {
 
-    public void EstimatePlaytime()
-    {
-        // Estimate the playtime of the player
-        // This is a very simple function that just adds up the time spent in each scene
-        // and outputs the result to the console.
+    // public void EstimatePlaytime()
+    // {
+    //     // Estimate the playtime of the player
+    //     // This is a very simple function that just adds up the time spent in each scene
+    //     // and outputs the result to the console.
         
-        float totalPlaytime = 0;
+    //     float totalPlaytime = 0;
 
-        foreach (float playtime in GameManager.instance.playtime)
-        {
-            totalPlaytime += playtime;
-        }
+    //     foreach (float playtime in GameManager.instance.playtime)
+    //     {
+    //         totalPlaytime += playtime;
+    //     }
 		
-        Console.WrtieLine("Total playtime: " + totalPlaytime + " seconds");
-    }
+    //     Console.WrtieLine("Total playtime: " + totalPlaytime + " seconds");
+    // }
 
-    public void SaveGame()
-    {
-        // Save the game
-        // This is a very simple function that just saves the current playtime of each scene
-        // to the GameManager.instance.playtime array.
-        // The GameManager.instance.playtime array is then saved to a file.
+    // public void SaveGame()
+    // {
+    //     // Save the game
+    //     // This is a very simple function that just saves the current playtime of each scene
+    //     // to the GameManager.instance.playtime array.
+    //     // The GameManager.instance.playtime array is then saved to a file.
         
-        for (int i = 0; i < GameManager.instance.playtime.Length; i++)
-        {
-            GameManager.instance.playtime[i] = GameManager.instance.playtime[i] + Time.timeSinceLevelLoad;
-        }
+    //     for (int i = 0; i < GameManager.instance.playtime.Length; i++)
+    //     {
+    //         GameManager.instance.playtime[i] = GameManager.instance.playtime[i] + Time.timeSinceLevelLoad;
+    //     }
 
-        GameManager.instance.Save();
-    }
+    //     GameManager.instance.Save();
+    // }
 
-    public void LoadGame()
-    {
-        // Load the game
-        // This is a very simple function that just loads the current playtime of each scene
-        // from the GameManager.instance.playtime array.
-        // The GameManager.instance.playtime array is then loaded from a file.
+    // public void LoadGame()
+    // {
+    //     // Load the game
+    //     // This is a very simple function that just loads the current playtime of each scene
+    //     // from the GameManager.instance.playtime array.
+    //     // The GameManager.instance.playtime array is then loaded from a file.
         
-        GameManager.instance.Load();
-    }
+    //     GameManager.instance.Load();
+    // }
 
     /* public void Jetpack()
     {
@@ -70,7 +71,7 @@ public class PlayerFunctions : MonoBehaviour
         GetComponent<Rigidbody>().useGravity = true;
         GetComponent<Jetpack>().enabled = false;
     } */
-    public void Controls()
+    /* public void Controls()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
@@ -90,7 +91,7 @@ public class PlayerFunctions : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.F3))
         {
             LoadGame();
-        }
+        } */
 
        /*  if (Input.GetKeyDown(KeyCode.F4))
         {
@@ -101,5 +102,5 @@ public class PlayerFunctions : MonoBehaviour
         {
             NormalMovement();
         } */
-    }
+    //}
 }
